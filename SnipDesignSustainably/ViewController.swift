532067a1751cb356a71d48ccbaf172c123076322
Kevin Jimenez <elestevy@gmail.com>
2020-12-28 08:54:46 -0800

    Initial Commit

---
+++ SnipDesignSustainably/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnipDesignSustainably
+//
+//  Created by Kevin Jimenez on 12/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
